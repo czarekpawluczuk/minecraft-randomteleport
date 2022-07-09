f961a490bd2d46aea5ffa707c2cc9136b6433028
czarru <makowatyc123@gmail.com>
2022-07-09 11:05:46 +0200

    fix errors

--- src/main/java/xyz/czarru/randomteleports/listeners/PlayerInteractListener.java
+++ src/main/java/xyz/czarru/randomteleports/listeners/PlayerInteractListener.java
@@ -35,36 +35,29 @@ public class PlayerInteractListener implements Listener {
         final Block clickedBlock = e.getClickedBlock();
         if(e.isCancelled())return;
         if(clickedBlock==null)return;
-        Bukkit.broadcastMessage("0");
         if(!clickedBlock.getState().getType().name().contains("BUTTON")) return;
-        Bukkit.broadcastMessage("01");
         final Button btn = (Button) e.getClickedBlock().getState().getData();
         final Block relativeBlock = clickedBlock.getRelative(btn.getAttachedFace());
         if(!a.equals(Action.RIGHT_CLICK_BLOCK))return;
-        Bukkit.broadcastMessage("1");
         final ConfigurationSection cfg = plugin.getConfig();
         if(!clickedBlock.getType().equals(Material.getMaterial(cfg.getString("config.blocks.button"))))return;
-        Bukkit.broadcastMessage("2");
         final Location randomLoc = coordinateHelper.getRandomLocation(p.getWorld());
-        Bukkit.broadcastMessage("02");
-        Bukkit.broadcastMessage("RETURN MATERIAL: "+Material.getMaterial(cfg.getString("config.blocks.solo")));
-        Bukkit.broadcastMessage("RETURN RELATIVE: "+relativeBlock.getType());
         if(relativeBlock.getType().equals(Material.getMaterial(cfg.getString("config.blocks.solo")))){
-            Bukkit.broadcastMessage("3");
             p.teleport(randomLoc);
             p.sendMessage(chatHelper.color(cfg.getString("config.messages.teleport")));
         }else if(relativeBlock.getType().equals(Material.getMaterial(cfg.getString("config.blocks.group")))){
             ArrayList<Player> nearbyPlayersList = coordinateHelper.getNearbyPlayers(relativeBlock.getLocation(), p);
-            if(nearbyPlayersList.isEmpty()) {
-                p.sendMessage(chatHelper.color(cfg.getString("config.messages.emptyList")));
-                return;
+            if(p.getLocation().getBlock().getType().equals(Material.getMaterial(plugin.getConfig().getString("config.blocks.plate")))) {
+                if (nearbyPlayersList.isEmpty()) {
+                    p.sendMessage(chatHelper.color(cfg.getString("config.messages.emptyList")));
+                }
+                Player opponent = nearbyPlayersList
+                        .stream()
+                        .findFirst()
+                        .get();
+                opponent.teleport(randomLoc);
+                p.teleport(randomLoc);
             }
-            Player opponent = nearbyPlayersList
-                    .stream()
-                    .findFirst()
-                    .get();
-            opponent.teleport(randomLoc);
-            p.teleport(randomLoc);
         }
     }
 }
